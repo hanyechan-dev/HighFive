8858bf05741ebc3b42848738f3bf6e3ecd221c38
tkd787878 <tkd787878@gmail.com>
2025-05-09 16:13:30 +0900

    AiConsulting, AiConsultingContent, CommonEnum, Consultant, ConsultantConsulting, CosnsultantConsultingContent 클래스 작성 완료

---
+++ backend/src/main/java/com/jobPrize/entity/AiConsulting.java
@@ -0,0 +1,53 @@
+package com.jobPrize.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ai_consulting")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AiConsulting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ai_consulting_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "request_document_id", nullable = false, unique = true)
+    private RequestDocument requestDocument;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CommonEnum.ConsultingType type;
+
+    @OneToMany(mappedBy = "aiConsulting", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<AiConsultingContent> contents = new ArrayList<>();
+    
+    @OneToOne(mappedBy = "aiConsulting", fetch = FetchType.LAZY)
+    private ConsultantConsulting consultantConsulting;
+
+    
+    
+}
