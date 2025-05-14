3d635276cff6a9606554fc189630fe0fa05e5f82
africasergei <yhcho4243@gmail.com>
2025-05-14 10:10:55 +0900

    오타 수정 및 테스트 환경설정 완료

--- backend/src/test/java/com/jobPrize/repository/common/UserRepositoryTest.java
+++ backend/src/test/java/com/jobPrize/repository/common/UserRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.jobPrize.repository.common;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,15 +11,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.Rollback;
 
 import com.jobPrize.config.QuerydslConfig;
-import com.jobPrize.entity.common.User;
-import com.jobPrize.entity.common.UserType;
-import com.jobPrize.entity.consultant.Consultant;
-import com.jobPrize.entity.memToCom.EducationLevel;
-import com.jobPrize.entity.member.Career;
 import com.jobPrize.entity.member.Certification;
-import com.jobPrize.entity.member.Education;
-import com.jobPrize.entity.member.Member;
 import com.jobPrize.entity.member.Resume;
+import com.jobPrize.repository.company.Industry.IndustryRepository;
+import com.jobPrize.repository.company.company.CompanyRepository;
 import com.jobPrize.repository.consultant.consultant.ConsultantRepository;
 import com.jobPrize.repository.member.career.CareerRepository;
 import com.jobPrize.repository.member.certification.CertificationRepository;
@@ -56,8 +50,11 @@ class UserRepositoryTest {
 	@Autowired
 	private CertificationRepository certificationRepository;
 
-//    @Autowired
-//    private CompanyRepository companyRepository;
+    @Autowired
+    private CompanyRepository companyRepository;
+    
+    @Autowired
+    private IndustryRepository industryRepository;
 
 	@Autowired
 	private EntityManager em;
@@ -85,101 +82,30 @@ class UserRepositoryTest {
 //
 //    }
 
-    @Test
-    @Rollback(false)
-    @DisplayName("User 및 Member 저장")
-    void saveUserAndMember() {
-        for(int i=0;i<1000;i++) {
-        	User user = User
-        			.builder()
-        			.email("memberTest"+i+"@user.com")
-                    .password("pass1234")
-                    .name("홍길동")
-                    .phone("0101234"+String.format("%04d", i))
-                    .address("서울특별시")
-                    .type(UserType.일반회원)
-                    .build();
-
-            userRepository.save(user);
-            
-            Member member = Member
-            		.builder()
-            		.user(user)
-            		.nickname("김탁봉")
-            		.build();
-            
-            memberRepository.save(member);
-        	
-        }
-        
-        em.flush();
-        em.clear();
-
-    }
-
-    @Test
-    @Rollback(false)
-    @DisplayName("User 및 Consultant 저장")
-    void saveUserAndConsultant() {
-        for(int i=0;i<1000;i++) {
-        	User user = User
-        			.builder()
-        			.email("consultantTest"+i+"@user.com")
-                    .password("pass1234")
-                    .name("김컨설")
-                    .phone("0101234"+String.format("%04d", i))
-                    .address("서울특별시")
-                    .type(UserType.컨설턴트회원)
-                    .build();
-
-            userRepository.save(user);
-            
-           Consultant consultant = Consultant
-            		.builder()
-            		.user(user)
-            		.build();
-            
-            consultantRepository.save(consultant);
-        	
-        }
-        
-        em.flush();
-        em.clear();
-
-    }
-
-//    @Test // 테스트 안해봄
+//    @Test
 //    @Rollback(false)
-//    @DisplayName("User 및 Company 저장")
-//    void saveUserAndConsultnat() {
+//    @DisplayName("User 및 Member 저장")
+//    void saveUserAndMember() {
 //        for(int i=0;i<1000;i++) {
 //        	User user = User
 //        			.builder()
-//        			.email("CompanyTest"+i+"@user.com")
+//        			.email("memberTest"+i+"@user.com")
 //                    .password("pass1234")
-//                    .name("박인사")
+//                    .name("홍길동")
 //                    .phone("0101234"+String.format("%04d", i))
-//                    .address("대전광역시")
-//                    .type(UserType.기업회원)
+//                    .address("서울특별시")
+//                    .type(UserType.일반회원)
 //                    .build();
 //
 //            userRepository.save(user);
 //            
-//           Company company = Company
+//            Member member = Member
 //            		.builder()
-//            		.companyName("기업"+i)
-//            		.representativeName("이대표")
-//            		.establishedDate(LocalDate.of(2005, 5, 3))
-//            		.businessNumber("111-11-"+String.format("%04d", i))
-//            		.companyAddress("동탄")
-//            		.type(CompanyType.중견기업)
-//            		.companyPhone("0104444"+String.format("%04d", i))
-//            		.introduction("문화를 선도하는 기업, 빙그레입니다.")
-//            		.employeeCount(155)
 //            		.user(user)
+//            		.nickname("김탁봉")
 //            		.build();
 //            
-//           companyRepository.save(company);
+//            memberRepository.save(member);
 //        	
 //        }
 //        
@@ -187,89 +113,184 @@ class UserRepositoryTest {
 //        em.clear();
 //
 //    }
-
-	@Test
-	@Rollback(false)
-	@DisplayName("Member에 Resume 저장")
-	void saveResume() {
-		for (int i = 1; i <= 1000; i++) {
-			Member member = memberRepository.findById(Long.valueOf(i)).orElseThrow();
-			Resume resume = Resume.builder().member(member).build();
-			
-			resumeRepository.save(resume);
-
-		}
-
-		em.flush();
-		em.clear();
-
-	}
 	
-	@Test
-	@Rollback(false)
-	@DisplayName("Resume에 Education 저장")
-	void saveEducation() {
-		for (int i = 1; i <= 1000; i++) {
-			Resume resume = resumeRepository.findById(Long.valueOf(i)).orElseThrow();
-			
-			for(int j=1 ; j<=5;j++) {
-				Education education = Education
-						.builder()
-						.resume(resume)
-						.schoolName("학교"+j)
-						.educationLevel(EducationLevel.학사)
-						.major("전공"+j)
-						.gpa(BigDecimal.valueOf(4.33))
-						.location("도시"+j)
-						.enterDate(LocalDate.of(2000+j*5, 3, 2))
-						.graduateDate(LocalDate.of(2005+j*5, 2, j))
-						.build();
-				
-				educationRepository.save(education);
-			}
-
-		}
-
-		em.flush();
-		em.clear();
-
-	}
+//
+//    @Test
+//    @Rollback(false)
+//    @DisplayName("User 및 Consultant 저장")
+//    void saveUserAndConsultant() {
+//        for(int i=0;i<1000;i++) {
+//        	User user = User
+//        			.builder()
+//        			.email("consultantTest"+i+"@user.com")
+//                    .password("pass1234")
+//                    .name("김컨설")
+//                    .phone("0101234"+String.format("%04d", i))
+//                    .address("서울특별시")
+//                    .type(UserType.컨설턴트회원)
+//                    .build();
+//
+//            userRepository.save(user);
+//            
+//           Consultant consultant = Consultant
+//            		.builder()
+//            		.user(user)
+//            		.build();
+//            
+//            consultantRepository.save(consultant);
+//        	
+//        }
+//        
+//        em.flush();
+//        em.clear();
+//
+//    }
 	
-	@Test
-	@Rollback(false)
-	@DisplayName("Resume에 Career 정보 저장")
-	void saveCareer() {
-		for (int i = 1; i <= 1000; i++) {
-			Resume resume = resumeRepository.findById(Long.valueOf(i)).orElseThrow();
-			
-			for(int j=1 ; j<=5;j++) {
-				Career career = Career
-						.builder()
-						.resume(resume)
-						.companyName("기업"+j)
-						.job("직무"+j)
-						.department("부서"+j)
-						.position("직급"+j)
-						.startDate(LocalDate.of(2000+j*5, 3, 2))
-						.endDate(LocalDate.of(2005+j*5, 3, 2))
-						.build();
-				
-				careerRepository.save(career);
-			}
+//  @Test
+//  @Rollback(false)
+//  @DisplayName("Industry 저장")
+//  void saveIndustry() {
+//      for(int i=0;i<20;i++) {
+//      	Industry industry = Industry
+//      			.builder()
+//      			.industryName("산업"+i)
+//      			.description("산업"+i+"에 대한 설명")
+//                 .build();
+//      	
+//      	industryRepository.save(industry);
+//     
+//      }
+//      
+//      em.flush();
+//  }
 
-		}
+//   @Test
+//   @Rollback(false)
+//   @DisplayName("User 및 Company 저장")
+//   void saveUserAndConsultnat() {
+//       for(int i=0;i<1000;i++) {
+//       	User user = User
+//       			.builder()
+//       			.email("CompanyTest"+i+"@user.com")
+//                   .password("pass1234")
+//                   .name("박인사")
+//                   .phone("0101234"+String.format("%04d", i))
+//                   .address("대전광역시")
+//                   .type(UserType.기업회원)
+//                   .build();
+//
+//           userRepository.save(user);
+//           
+//           Industry industry = industryRepository.findById(Long.valueOf(i%20+1)).orElseThrow(() -> new IllegalArgumentException("해당 ID의 멤버가 존재하지 않음"));
+//           
+//          Company company = Company
+//           		.builder()
+//           		.companyName("기업"+i)
+//           		.representativeName("이대표")
+//           		.establishedDate(LocalDate.of(2005, 5, 3))
+//           		.businessNumber("111-11-"+String.format("%04d", i))
+//           		.companyAddress("동탄")
+//           		.type(CompanyType.중견기업)
+//           		.industry(industry)
+//           		.companyPhone("0104444"+String.format("%04d", i))
+//           		.introduction("문화를 선도하는 기업, 빙그레입니다.")
+//           		.employeeCount(155)
+//           		.user(user)
+//           		.build();
+//           
+//          companyRepository.save(company);
+//       	
+//       }
+//       
+//       em.flush();
+//       em.clear();
+//
+//   }
 
-		em.flush();
-		em.clear();
 
-	}
+//	@Test
+//	@Rollback(false)
+//	@DisplayName("Member에 Resume 저장")
+//	void saveResume() {
+//		for (int i = 10001; i <= 11000; i++) {
+//			Member member = memberRepository.findById(Long.valueOf(i)).orElseThrow(() -> new IllegalArgumentException("해당 ID의 멤버가 존재하지 않음"));
+//			Resume resume = Resume.builder().member(member).build();
+//			
+//			resumeRepository.save(resume);
+//
+//		}
+//
+//		em.flush();
+//		em.clear();
+//
+//	}
+
 	
+//	@Test
+//	@Rollback(false)
+//	@DisplayName("Resume에 Education 저장")
+//	void saveEducation() {
+//		for (int i = 1; i <= 1000; i++) {
+//			Resume resume = resumeRepository.findById(Long.valueOf(i)).orElseThrow(() -> new IllegalArgumentException("해당 ID의 멤버가 존재하지 않음"));
+//			
+//			for(int j=1 ; j<=5;j++) {
+//				Education education = Education
+//						.builder()
+//						.resume(resume)
+//						.schoolName("학교"+j)
+//						.educationLevel(EducationLevel.학사)
+//						.major("전공"+j)
+//						.gpa(BigDecimal.valueOf(4.33))
+//						.location("도시"+j)
+//						.enterDate(LocalDate.of(2000+j*5, 3, 2))
+//						.graduateDate(LocalDate.of(2005+j*5, 2, j))
+//						.build();
+//				
+//				educationRepository.save(education);
+//			}
+//
+//		}
+//
+//		em.flush();
+//		em.clear();
+//
+//	}
+//	
+//	@Test
+//	@Rollback(false)
+//	@DisplayName("Resume에 Career 정보 저장")
+//	void saveCareer() {
+//		for (int i = 1; i <= 1000; i++) {
+//			Resume resume = resumeRepository.findById(Long.valueOf(i)).orElseThrow(() -> new IllegalArgumentException("해당 ID의 멤버가 존재하지 않음"));
+//			
+//			for(int j=1 ; j<=5;j++) {
+//				Career career = Career
+//						.builder()
+//						.resume(resume)
+//						.companyName("기업"+j)
+//						.job("직무"+j)
+//						.department("부서"+j)
+//						.position("직급"+j)
+//						.startDate(LocalDate.of(2000+j*5, 3, 2))
+//						.endDate(LocalDate.of(2005+j*5, 3, 2))
+//						.build();
+//				
+//				careerRepository.save(career);
+//			}
+//
+//		}
+//
+//		em.flush();
+//		em.clear();
+//
+//	}
+//	
 	@Test
 	@Rollback(false)
 	@DisplayName("Resume에 Ceritication 정보 저장")
 	void saveCeritication() {
 		for (int i = 1; i <= 1000; i++) {
-			Resume resume = resumeRepository.findById(Long.valueOf(i)).orElseThrow();
+			Resume resume = resumeRepository.findById(Long.valueOf(i)).orElseThrow(() -> new IllegalArgumentException("해당 ID의 멤버가 존재하지 않음"));
 			
 			for(int j=1 ; j<=5;j++) {
 				Certification certification = Certification
@@ -279,6 +300,7 @@ class UserRepositoryTest {
 						.issuingOrg("발급기관"+i)
 						.score(String.valueOf(i))
 						.acquisitionDate(LocalDate.of(2005+j*5, 3, 2))
+						.certificationNo("100000"+i+j)
 						.build();
 				
 				certificationRepository.save(certification);
