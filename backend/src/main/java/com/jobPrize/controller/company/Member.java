c9e1190656ea0216bb9fbac3c478cf255722046d
kws980223 <kws980223@gmail.com>
2025-05-27 23:40:27 +0900

    멤버풀컨트롤러

---
+++ backend/src/main/java/com/jobPrize/controller/company/Member.java
@@ -0,0 +1,38 @@
+package com.jobPrize.controller.company;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jobPrize.dto.company.memberPool.MemberFilterCondition;
+import com.jobPrize.dto.company.memberPool.MemberPoolSummaryDto;
+import com.jobPrize.service.company.memberPool.MemberPoolService;
+import com.jobPrize.util.SecurityUtil;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("company/members")
+@RequiredArgsConstructor
+
+	public class Member {
+
+    private final MemberPoolService memberPoolService ;
+
+    @PostMapping
+    public ResponseEntity<Page<MemberPoolSummaryDto>> readMembers(@RequestBody @Valid MemberFilterCondition condition, Pageable pageable) {
+
+        Long id = SecurityUtil.getId();
+
+        Page<MemberPoolSummaryDto> memberPoolSummaryDtos = memberPoolService.readMemberPoolPageByCondition(id, condition, pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(memberPoolSummaryDtos);
+    }
+
+}
