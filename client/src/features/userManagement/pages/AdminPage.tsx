552fdf731b7a24c62519615028caf789ccac8af3
sergei <yhcho4243@gmail.com>
2025-07-04 15:44:14 +0900

    디버깅

--- client/src/features/userManagement/pages/AdminPage.tsx
+++ client/src/features/userManagement/pages/AdminPage.tsx
@@ -13,6 +13,10 @@ function AdminPage() {
         api(true).post("/admin/similarity");
     }
 
+    const onClickGetMemberVector = () =>{
+        api(true).post("/admin/member-vector")
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-white pb-[40px]">
             <img src="/jobPrize.png" className="w-[360px] h-auto mb-6" />
@@ -32,6 +36,9 @@ function AdminPage() {
                     <AdminCard icon={<SettingsIcon />} label={"프롬프트 설정"} />
                 </div>
             </div>
+
+            <button className="px-6 py-2 rounded-lg cursor-pointer w-[730px] text-center mb-6" onClick={onClickGetMemberVector}>
+            </button>
 
             <button className="px-6 py-2 rounded-lg cursor-pointer w-[730px] text-center" onClick={onClickGetSimilarity}>
             </button>
